339412a38d4807ea2e8b50a721f730a79e9344b4
Tom Golebiewski <tgolebie16@gmail.com>
2023-08-12 05:57:11 -0400

    lab done

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(a, b) {
+    return a + b;
+}
+    function increment(num) {
+      return add(num, 1)
+   }
+function subtract(a, b) {
+  return a - b;
+}
+    function decrement(num) {
+      return subtract(num, 1)
+    }
+    function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+function makeInt(string) {
+  return parseInt(string, 10);
+}
+function preserveDecimal(string) {
+  return parseFloat(string);
+}
